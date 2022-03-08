83c3ffc2a883c1b38050587237fc667f6c9b419e
Tom <tgraham@wpi.edu>
2022-03-08 12:51:40 -0500

    started online attack code

--- hw2.py
+++ hw2.py
@@ -3,19 +3,20 @@
 # Advanced Crypto
 # Project 2
 
-import random
 import hashlib
 import hmac
 import secrets
 
-# Calculate hash for given x
+# Calculate hash for given input
 def calculate_hash(plain_text, key):
 	cipher_text = hmac.new(key.encode(), msg=plain_text, digestmod=hashlib.sha256).digest()[0:2]
 	key = cipher_text
 	return key.hex()
 
 # Precompute each chain
-def compute_chain(plain_text, SP):
+def compute_chain(plain_text, SP, found):
+	if (found):
+		chain_length = chain_length - 1
 	j = 0
 	InitalSP = SP
 	chain_length = 2**8
@@ -25,7 +26,7 @@ def compute_chain(plain_text, SP):
 		j = j + 1
 	dict[SP] = InitalSP
 
-# Precompute the table
+# Precompute the table and return it as a dictionary
 def compute_table(plain_text, chain_length, k, bytes):
 	i = 0
 	keyspace = 2**k
@@ -33,23 +34,21 @@ def compute_table(plain_text, chain_length, k, bytes):
 
 	while (i < num_of_chains):
 		SP = secrets.token_hex(nbytes=bytes)
-		compute_chain(plain_text, SP)
+		compute_chain(plain_text, SP, False)
 		i = i + 1
 	return dict
-	#online_attack(dict, SP, InitalSP)
 
-# The online stage find preimage, given y find x
-def online_attack(dict, SP):
-	print('seraching')
-	# find pre-image by using table
-	# while SP not in dict:
-	# 	compute_table(plain_text, chain_length, k, bytes)
-	# 	print ('collision found')
-	# return True
-
-	# if dictionary has SP for given EP
-	# use distingued point technique
-	# pre_image = 
+# Given cipher text(y) find key that encrypted plain text
+def online_attack(dict, y):
+	# check y value against EP
+	# if it matches an EP that means the key is the previous entry in the chain
+    if y in dict.keys():
+        print("Y =", dict[y])
+		# feed y as input to hash and stop one spot before the end of the chain
+		# compute_chain(plain_text, y, True)
+	# else:
+	# 	Rehash y into yprime then check
+	# 	loop this
 
 
 if __name__ == "__main__":
@@ -64,7 +63,8 @@ if __name__ == "__main__":
 	chain_length = 2**8
 	plain_text = "Testing".encode()
 	dict = compute_table(plain_text, chain_length, k, bytes)
-	print (dict)
+	online_attack(dict, '4')
+	# print (dict)
 
 	# Case B
 	# 20 bits
